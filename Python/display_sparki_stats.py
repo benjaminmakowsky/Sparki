22c4f9f557befa11c320e5e1010011147863d47d
Ben <benjamin.makowsky@gmail.com>
2020-08-29 23:27:09 -0700

    Write battery level to database

--- Python/display_sparki_stats.py
+++ Python/display_sparki_stats.py
@@ -1,55 +1,61 @@
 import mysql.connector
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
 import asyncio
 from sphero_sdk import SpheroRvrAsync
 from sphero_sdk import SerialAsyncDal
 from sphero_sdk import BatteryVoltageStatesEnum as VoltageStates
 
-#Define variables
-DATABASE_NAME = "SPARKI"
 loop = asyncio.get_event_loop()
 rvr = SpheroRvrAsync(dal=SerialAsyncDal(loop))
-
-#Connect to mysql
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="password"
-)
-
-#A cursor is just an object that lets you interact with a database, like a mouse -> computer
-mycursor = mydb.cursor()
-
-#Check if the database already exists
-db_exists = False
-databases = mycursor.fetchall()
-for db in databases: #db is a tuple with only a single element so we need to use indexing
-    if (db[0] == DATABASE_NAME):
-        db_exists = True
-
-# if databse does not exist create it
-if(db_exists == False):
-    print("Database not found, Creating now...")
-    mycursor.execute("CREATE DATABASE " + DATABASE_NAME)     
-else:
-    #If database exists, wipe it to start fresh
-    print("Database already exists. Deleting and Recreating")
-    mycursor.execute("DROP DATABASE " + DATABASE_NAME)
-    mycursor.execute("CREATE DATABASE " + DATABASE_NAME)     
-
-#Connect to the database
-print("Connecting to DB: " + DATABASE_NAME)
-mydb.connect(database=DATABASE_NAME)
-
-#Creating table meesages with cells msg
-mycursor.execute("CREATE TABLE stats (battery VARCHAR(255))")
 
 async def main():
     """ This program demonstrates how to retrieve the battery state of RVR.
     """
+
+
+    #Define variables
+    DATABASE_NAME = "SPARKI"
+    
+
+    #Connect to mysql
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="YOUR_PASSWORD_HERE"
+    )
+
+    #A cursor is just an object that lets you interact with a database, like a mouse -> computer
+    mycursor = mydb.cursor()
+    mycursor.execute("SHOW DATABASES")
+
+    #Check if the database already exists
+    db_exists = False
+    databases = mycursor.fetchall()
+    for db in databases: #db is a tuple with only a single element so we need to use indexing
+        if (db[0] == DATABASE_NAME):
+            db_exists = True
+
+    # if databse does not exist create it
+    if(db_exists == False):
+        print("Database not found, Creating now...")
+        mycursor.execute("CREATE DATABASE " + DATABASE_NAME)     
+    else:
+        #If database exists, wipe it to start fresh
+        print("Database already exists. Deleting and Recreating")
+        mycursor.execute("DROP DATABASE " + DATABASE_NAME)
+        mycursor.execute("CREATE DATABASE " + DATABASE_NAME)     
+
+    #Connect to the database
+    print("Connecting to DB: " + DATABASE_NAME)
+    mydb.connect(database=DATABASE_NAME)
+
+    #Creating table meesages with cells msg
+    print("Creating Table: stats with cell battery")
+    mycursor.execute("CREATE TABLE stats (battery VARCHAR(255))")
+
 
     await rvr.wake()
 
@@ -64,8 +70,15 @@ async def main():
 
 
     ## Store battery level in database
-    mycursor.execute("INSERT INTO stats (battery) VALUES (%s)", battery_percentage)
+    query = "INSERT INTO SPARKI.stats (battery) VALUES (%s)"
+    values= ("Battery Level: " + str(battery_percentage.get("percentage")),)
+    mycursor.execute(query, values)
     mydb.commit()
+
+    mycursor.execute("SELECT * FROM SPARKI.stats")
+    records = mycursor.fetchall() ## it returns list of tables present in the database
+    for record in records:
+        print(record)
 
     # state_info = '[{}, {}, {}, {}]'.format(
     #     '{}: {}'.format(VoltageStates.unknown.name, VoltageStates.unknown.value),
