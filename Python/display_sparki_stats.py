76cf2266840259d03cd04227cd73656af46a0f32
Ben <benjamin.makowsky@gmail.com>
2020-08-30 00:33:24 -0700

    updated code

--- Python/display_sparki_stats.py
+++ Python/display_sparki_stats.py
@@ -11,51 +11,51 @@ from sphero_sdk import BatteryVoltageStatesEnum as VoltageStates
 loop = asyncio.get_event_loop()
 rvr = SpheroRvrAsync(dal=SerialAsyncDal(loop))
 
+
+#Define variables
+DATABASE_NAME = "SPARKI"
+
+
+#Connect to mysql
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="YOUR_PASSWORD_HERE" #CHANGE THIS PASSWORD
+)
+
+#A cursor is just an object that lets you interact with a database, like a mouse -> computer
+mycursor = mydb.cursor()
+mycursor.execute("SHOW DATABASES")
+
+#Check if the database already exists
+db_exists = False
+databases = mycursor.fetchall()
+for db in databases: #db is a tuple with only a single element so we need to use indexing
+    if (db[0] == DATABASE_NAME):
+        db_exists = True
+
+# if databse does not exist create it
+if(db_exists == False):
+    print("Database not found, Creating now...")
+    mycursor.execute("CREATE DATABASE " + DATABASE_NAME)     
+else:
+    #If database exists, wipe it to start fresh
+    print("Database already exists. Deleting and Recreating")
+    mycursor.execute("DROP DATABASE " + DATABASE_NAME)
+    mycursor.execute("CREATE DATABASE " + DATABASE_NAME)     
+
+#Connect to the database
+print("Connecting to DB: " + DATABASE_NAME)
+mydb.connect(database=DATABASE_NAME)
+
+#Create a table called: stats with a cell called battery
+print("Creating Table: stats with a cell: battery")
+mycursor.execute("CREATE TABLE stats (battery VARCHAR(255))")
+
+
 async def main():
     """ This program demonstrates how to retrieve the battery state of RVR.
     """
-
-
-    #Define variables
-    DATABASE_NAME = "SPARKI"
-    
-
-    #Connect to mysql
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="YOUR_PASSWORD_HERE"
-    )
-
-    #A cursor is just an object that lets you interact with a database, like a mouse -> computer
-    mycursor = mydb.cursor()
-    mycursor.execute("SHOW DATABASES")
-
-    #Check if the database already exists
-    db_exists = False
-    databases = mycursor.fetchall()
-    for db in databases: #db is a tuple with only a single element so we need to use indexing
-        if (db[0] == DATABASE_NAME):
-            db_exists = True
-
-    # if databse does not exist create it
-    if(db_exists == False):
-        print("Database not found, Creating now...")
-        mycursor.execute("CREATE DATABASE " + DATABASE_NAME)     
-    else:
-        #If database exists, wipe it to start fresh
-        print("Database already exists. Deleting and Recreating")
-        mycursor.execute("DROP DATABASE " + DATABASE_NAME)
-        mycursor.execute("CREATE DATABASE " + DATABASE_NAME)     
-
-    #Connect to the database
-    print("Connecting to DB: " + DATABASE_NAME)
-    mydb.connect(database=DATABASE_NAME)
-
-    #Creating table meesages with cells msg
-    print("Creating Table: stats with cell battery")
-    mycursor.execute("CREATE TABLE stats (battery VARCHAR(255))")
-
 
     await rvr.wake()
 
@@ -80,14 +80,6 @@ async def main():
     for record in records:
         print(record)
 
-    # state_info = '[{}, {}, {}, {}]'.format(
-    #     '{}: {}'.format(VoltageStates.unknown.name, VoltageStates.unknown.value),
-    #     '{}: {}'.format(VoltageStates.ok.name, VoltageStates.ok.value),
-    #     '{}: {}'.format(VoltageStates.low.name, VoltageStates.low.value),
-    #     '{}: {}'.format(VoltageStates.critical.name, VoltageStates.critical.value)
-    # )
-    # print('Voltage states: ', state_info)
-
     await rvr.close()
 
 
@@ -99,6 +91,14 @@ if __name__ == '__main__':
 
     except KeyboardInterrupt:
         print('\nProgram terminated with keyboard interrupt.')
+
+        loop.run_until_complete(
+            rvr.close()
+        )
+
+    finally:
+        if loop.is_running():
+            loop.close() terminated with keyboard interrupt.')
 
         loop.run_until_complete(
             rvr.close()
