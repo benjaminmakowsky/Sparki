63d23b536542304eff6164ae1ecd4635d7bb0ffc
Benjamin Makowsky <benjamin.makowsky@gmail.com>
2020-09-08 13:36:48 -0700

    Begin work on ambient light sensor

--- sparki-web-app/src/DataBaseViewer.tsx
+++ sparki-web-app/src/DataBaseViewer.tsx
@@ -4,21 +4,22 @@ import './DataTable.css';
 interface Props{
     battery: String
     heading: String
+    light: String
 }
 
 
-export const DatabaseView: React.FC<Props> = ({battery, heading}) => {
+export const DatabaseView: React.FC<Props> = ({battery, heading, light}) => {
     return(
       <table className={"DataTable"}>
         <tr>
           <th className={"LeftTableHeader"}>Battery</th>
           <th className={"CenterTableHeader"}>Heading</th>
-          <th className={"RightTableHeader"}>Voltage</th>
+          <th className={"RightTableHeader"}>Ambient Light</th>
         </tr>
         <tr>
           <td className={"TableData"}>{battery}</td>
           <td className={"TableData"}>{heading}</td>
-          <td className={"TableData"}>N/A</td>
+          <td className={"TableData"}>{light}</td>
         </tr>
       </table>
   );
